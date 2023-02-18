fb9b28d3f19d058a608c3af6ce091a7251d0e919
Dasom Heo <dasomee@outlook.com>
2023-02-18 18:01:24 -0500

    evaluator() checks win condition when move is made. Tells pubsub that turn ends, winner found or draw.

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const gameBoard = (() => {
     boardState[ind] = symbol;
     render();
   }
-  pubsub.subscription("endTurn", update).subscribe();
+  pubsub.subscription("boardChange", update).subscribe();
 
   function resetBoardState() {
     boardState = [0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -27,6 +27,8 @@ const gameBoard = (() => {
   function getState() {
     return [...boardState];
   }
+
+  render();
 
   return { getState };
 })();
@@ -67,10 +69,85 @@ const clickHandler = (() => {
     const secondCondition = !(
       e.target.classList.contains("x") || e.target.classList.contains("o")
     );
-    if (firstCondition && secondCondition)
-      pubsub.publish("endTurn", squareID, currentPlayer);
-      console.log(gameBoard.getState())
+    if (firstCondition && secondCondition) {
+      pubsub.publish("boardChange", squareID, currentPlayer);
+    }
   });
 })();
 
+const evaluator = (() => {
+  function getInd(myArr, myVal) {
+    return myArr.reduce((accumulator, currentValue, index) => {
+      if (currentValue === myVal) accumulator.push(index);
+      return accumulator;
+    }, []);
+  }
 
+  const winConditionGenerator = (nArray, a) => {
+    // Win Condition Formula: [n] [n+a] [n+2a] where n = [0,3,6] and a = 1
+    // Columns: [0,1,2], a=3; Corner1: [0], a=4; Corner2: [2], a=2;
+    const output = [];
+    nArray.forEach((n) => {
+      output.push([n, n + a, n + 2 * a]);
+    });
+    return output;
+  };
+
+  const rowConds = winConditionGenerator([0, 3, 6], 1);
+  const colConds = winConditionGenerator([0, 1, 2], 3);
+  const cor1Conds = winConditionGenerator([0], 4);
+  const cor2Conds = winConditionGenerator([2], 2);
+  const winConditions = [...rowConds, ...colConds, ...cor1Conds, ...cor2Conds];
+
+  function condFilter(matchedArray, winConditions) {
+    const counter = {};
+    matchedArray.forEach((value) => {
+      winConditions.forEach((condition, index) => {
+        if (condition.includes(value)) {
+          // console.log({condition, value})
+          if (counter.hasOwnProperty(index)) counter[index]++;
+          else counter[index] = 1;
+        }
+      });
+    });
+    return counter;
+  }
+
+  function checkWin() {
+    const marker = gameMaster.getCurrentPlayer();
+    const playerInd = getInd(gameBoard.getState(), marker);
+    const playerProgress = condFilter(playerInd, winConditions);
+    let isCurrentPlayerWinner = false;
+
+    Object.keys(playerProgress).forEach((key) => {
+      if (playerProgress[key] > 2) {
+        isCurrentPlayerWinner = true;
+        pubsub.publish("winnerFound");
+      }
+    });
+
+    if (
+      !isCurrentPlayerWinner &&
+      getInd(gameBoard.getState(), 0).length === 0
+    ) {
+      pubsub.publish("draw");
+    }
+
+    pubsub.publish("endTurn");
+  }
+  pubsub.subscription("boardChange", checkWin).subscribe();
+})();
+
+const displayController = (() => {
+  const myP = document.querySelector(".winner");
+
+  function announceWinner() {
+    myP.innerHTML = `Winner is ${gameMaster.getCurrentPlayer()}`;
+  }
+  pubsub.subscription("winnerFound", announceWinner).subscribe();
+
+  function announceDraw() {
+    myP.innerHTML = "It's a draw...";
+  }
+  pubsub.subscription("draw", announceDraw).subscribe();
+})();
